947ccfcd2cd37808a9e5f7ee164064a2e09cdeb0
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-06-06 17:07:47 +0400

    Добавлена функция randomFloatFromInterval

--- js/main.js
+++ js/main.js
@@ -5,9 +5,20 @@ function randomIntFromInterval (min, max) {
   if (min < 0 || max < 0) {
     return Error('В функции randomIntFromInterval передан аргумент меньше нуля');
   }
-  // Подсмотрено на https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+  // Формула взята из https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function randomFloatFromInterval (min, max, counter = 2) {
+  if (min >= max) {
+    return Error('В функции randomFloatFromInterval начальное число больше или равно конечному');
+  }
+  if (min < 0 || max < 0) {
+    return Error('В функции randomFloatFromInterval передан аргумент меньше нуля');
+  }
 
-randomIntFromInterval (1, 10);
+  return +(Math.random() * (max - min) + min).toFixed(counter);
+}
+
+randomIntFromInterval(0,10);
+randomFloatFromInterval(100,10000);
