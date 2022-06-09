4071cfb14b16cc57ef63538da6c00708bbd2e40c
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-06-09 14:56:24 +0400

    Добавлена вспомогательная функция checkArguments, return заменён на throw

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-function randomIntFromInterval (min, max) {
+function checkArguments (min, max, functionName) {
   if (min >= max) {
-    return Error('В функции randomIntFromInterval начальное число больше или равно конечному');
+    throw Error(`В функции ${functionName} начальное число больше или равно конечному`);
   }
   if (min < 0 || max < 0) {
-    return Error('В функции randomIntFromInterval передан аргумент меньше нуля');
+    throw Error(`В функции ${functionName} передан аргумент меньше нуля`);
   }
+}
+
+function randomIntFromInterval (min, max) {
+  checkArguments (min, max, 'randomIntFromInterval');
   // Формула взята из https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomFloatFromInterval (min, max, counter = 2) {
-  if (min >= max) {
-    return Error('В функции randomFloatFromInterval начальное число больше или равно конечному');
-  }
-  if (min < 0 || max < 0) {
-    return Error('В функции randomFloatFromInterval передан аргумент меньше нуля');
-  }
+  checkArguments (min, max, 'randomFloatFromInterval');
 
   return +(Math.random() * (max - min) + min).toFixed(counter);
 }
 
-randomIntFromInterval(0,10);
+randomIntFromInterval(1,10);
 randomFloatFromInterval(100,10000);
