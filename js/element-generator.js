d9bf578e1100b72a8a46ab1466b6557e2ebb5d60
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-06-30 01:25:54 +0400

    Добавлены проверки на наличие контента

--- js/element-generator.js
+++ js/element-generator.js
@@ -12,19 +12,37 @@ descriptions.forEach(({author, offer}) => {
   const cardElement = cardTemplate.cloneNode(true);
 
   const fillContent = (selector, content) => {
-    cardElement.querySelector(selector).textContent = content;
+    if (content) {
+      cardElement.querySelector(selector).textContent = content;
+    } else {
+      cardElement.querySelector(selector).remove();
+    }
   };
 
   fillContent('.popup__title', offer.title);
   fillContent('.popup__text--address', offer.address);
   fillContent('[data-price]', offer.price);
-  fillContent('.popup__text--capacity',
-    `${offer.rooms} комнат${(offer.rooms === 1) ? 'a' : 'ы'} для ${offer.guests} гост${(offer.guests === 1) ? 'я' : 'ей'}`);
-  fillContent('.popup__text--time',
-    `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
-  cardElement.querySelector('.popup__description', offer.description);
+  fillContent('.popup__description', offer.description);
 
-  cardElement.querySelector('.popup__avatar').src = author.avatar;
+  if (author.avatar) {
+    cardElement.querySelector('.popup__avatar').src = author.avatar;
+  } else {
+    cardElement.querySelector('.popup__avatar').remove();
+  }
+
+  if (offer.rooms && offer.guests) {
+    fillContent('.popup__text--capacity',
+      `${offer.rooms} комнат${(offer.rooms === 1) ? 'a' : 'ы'} для ${offer.guests} гост${(offer.guests === 1) ? 'я' : 'ей'}`);
+  } else {
+    cardElement.querySelector('.popup__text--capacity').remove();
+  }
+
+  if (offer.checkin && offer.checkout) {
+    fillContent('.popup__text--time',
+      `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
+  } else {
+    cardElement.querySelector('.popup__text--time').remove();
+  }
 
   switch (offer.type) {
     case 'flat':
@@ -47,29 +65,33 @@ descriptions.forEach(({author, offer}) => {
   }
 
   const featuresList = cardElement.querySelectorAll('.popup__feature');
-  featuresList.forEach((featureListItem) => {
-    const isFeature = offer.features.some((offerFeature) => featureListItem.classList.contains(`popup__feature--${offerFeature}`));
-    if (!isFeature) {
-      featureListItem.remove();
-    }
-  });
+  if (offer.features && offer.features.length !== 0) {
+    featuresList.forEach((featureListItem) => {
+      const isFeature = offer.features.some((offerFeature) => featureListItem.classList.contains(`popup__feature--${offerFeature}`));
+      if (!isFeature) {
+        featureListItem.remove();
+      }
+    });
+  } else {
+    cardElement.querySelector('.popup__features').remove();
+  }
 
   const popupPhotos = cardElement.querySelector('.popup__photos');
-  const imageTemplate = popupPhotos.querySelector('.popup__photo');
-  const imagesFragment = document.createDocumentFragment();
-  popupPhotos.textContent = '';
-  offer.photos.forEach((photo) => {
-    const imageElement = imageTemplate.cloneNode(true);
-    imageElement.src = photo;
-    imagesFragment.append(imageElement);
-  });
-  popupPhotos.append(imagesFragment);
-
-  // Предусмотрите ситуацию, когда данных для заполнения не хватает. Например, отсутствует описание. В этом случае соответствующий блок в карточке скрывается.
+  if (offer.photos && offer.photos.length !== 0) {
+    const imageTemplate = popupPhotos.querySelector('.popup__photo');
+    const imagesFragment = document.createDocumentFragment();
+    popupPhotos.textContent = '';
+    offer.photos.forEach((photo) => {
+      const imageElement = imageTemplate.cloneNode(true);
+      imageElement.src = photo;
+      imagesFragment.append(imageElement);
+    });
+    popupPhotos.append(imagesFragment);
+  } else {
+    popupPhotos.remove();
+  }
 
   descriptionListFragment.append(cardElement);
 });
 
 mapBlock.append(descriptionListFragment.firstChild);
-
-console.log(descriptions, descriptionListFragment);
