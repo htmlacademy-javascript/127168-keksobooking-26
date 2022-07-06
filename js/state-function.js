1def7853099160463161027a8131cbc55199852a
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-07 00:55:00 +0400

    Добавлена функция changeDisabledFormStatus

--- js/state-function.js
+++ js/state-function.js
@@ -3,27 +3,23 @@ const mapFiltersElement = document.querySelector('.map__filters');
 const adFormChildren = adFormBlock.children;
 const mapFiltersChildren = mapFiltersElement.children;
 
-const setInactiveState = () => {
-  for (const child of adFormChildren) {
-    child.disabled = 'disabled';
+const changeDisabledFormStatus = (elements, status) => {
+  for (const element of elements) {
+    element.disabled = status;
   }
+};
 
-  for (const child of mapFiltersChildren) {
-    child.disabled = 'disabled';
-  }
+const setInactiveState = () => {
+  changeDisabledFormStatus(adFormChildren, 'disabled');
+  changeDisabledFormStatus(mapFiltersChildren, 'disabled');
 
   adFormBlock.classList.add('ad-form--disabled');
   mapFiltersElement.classList.add('map__filters--disabled');
 };
 
 const setActiveState = () => {
-  for (const child of adFormChildren) {
-    child.disabled = '';
-  }
-
-  for (const child of mapFiltersChildren) {
-    child.disabled = '';
-  }
+  changeDisabledFormStatus(adFormChildren, '');
+  changeDisabledFormStatus(mapFiltersChildren, '');
 
   adFormBlock.classList.remove('ad-form--disabled');
   mapFiltersElement.classList.remove('map__filters--disabled');
