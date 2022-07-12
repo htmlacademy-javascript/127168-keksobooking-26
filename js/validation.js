7622cf39bad5b7331f46fac4799d12d453890cad
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-12 14:54:47 +0400

    Добавлены сообщения об ошибке

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,5 @@
+import {correctEndOfWord} from './util.js';
+
 const COMMERCIAL_ROOM_NUM = 100;
 const COMMERTIAL_GUEST_NUM = 0;
 
@@ -16,6 +18,22 @@ const validateCapacityRoom = () => {
   return +capacityField.value <= +roomNumberField.value;
 };
 
+const getCapacityRoomErrorMessage = () => {
+  const numOfRooms = roomNumberField.value;
+  const roomWord = correctEndOfWord(numOfRooms, 'комнат', ['ы', '', '']);
+  const guestWord = correctEndOfWord(numOfRooms, 'гост', ['я', 'ей', 'ей']);
+
+  if (+capacityField.value !== COMMERTIAL_GUEST_NUM && +roomNumberField.value !== COMMERCIAL_ROOM_NUM) {
+    return `Для ${numOfRooms} ${roomWord} не больше ${numOfRooms} ${guestWord}`;
+  }
+
+  if (+capacityField.value === COMMERTIAL_GUEST_NUM) {
+    return 'Не для гостей только 100 комнат';
+  }
+
+  return '100 комнат не для гостей';
+};
+
 const pristine = new Pristine(adForm, {
   classTo: 'ad-form__element',
   errorTextParent: 'ad-form__element',
@@ -23,7 +41,7 @@ const pristine = new Pristine(adForm, {
   errorTextClass: 'ad-form__element--error'
 });
 
-pristine.addValidator(roomNumberField, validateCapacityRoom, 'плохо, лови здесь длинный комментарий, вот прям очень длинный комментарий');
+pristine.addValidator(roomNumberField, validateCapacityRoom, getCapacityRoomErrorMessage);
 
 adForm.addEventListener ('submit', (evt) => {
   evt.preventDefault();
