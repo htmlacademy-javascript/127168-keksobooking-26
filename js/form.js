574941b58e3c93ba79ed306db2af52e66af8d311
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-24 19:29:56 +0400

    Кнопка отправить заблокирована во время отправки

--- js/form.js
+++ js/form.js
@@ -8,18 +8,38 @@ const RESET_PRICE_VALUE = 1000;
 const adForm = document.querySelector('.ad-form');
 const filterForm = document.querySelector('.map__filters');
 const priceField = adForm.querySelector('#price');
+const submitButton = adForm.querySelector('.ad-form__submit');
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.style.backgroundColor = 'LightGrey';
+  submitButton.textContent = 'Отправляю...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.style.backgroundColor = 'white';
+  submitButton.textContent = 'Опубликовать';
+};
 
 const makeSuccessfulAction = () => {
   adForm.reset();
   filterForm.reset();
+  unblockSubmitButton();
   showSuccessMessage();
+};
+
+const makeErrorAction = () => {
+  unblockSubmitButton();
+  showErrorMessage();
 };
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   if (pristine.validate()) {
+    blockSubmitButton();
     const formData = new FormData(adForm);
-    postData(makeSuccessfulAction, showErrorMessage, formData);
+    postData(makeSuccessfulAction, makeErrorAction, formData);
   }
 });
 
