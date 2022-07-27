86722da393ec6e07cb5df5da01b28842020896f4
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-27 13:22:21 +0400

    Обработчик событий выведен в отдельную функцию и в экспорт

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,23 @@
-const filterForm = document.querySelector('.map__filters');
+const initFilterEventListener = () => {
+  const filterForm = document.querySelector('.map__filters');
 
-filterForm.addEventListener('change', (evt) => {
-  if (evt.target.id === 'housing-type') {
-    console.log('Тип дома');
-  }
-  if (evt.target.id === 'housing-price') {
-    console.log('Стоимость аренды');
-  }
-  if (evt.target.id === 'housing-rooms') {
-    console.log('Количество комнат');
-  }
-  if (evt.target.id === 'housing-guests') {
-    console.log('Количество гостей');
-  }
-  if (evt.target.parentElement.id === 'housing-features') {
-    console.log('Фишечки');
-  }
-  // console.dir(evt.target);
-});
+  filterForm.addEventListener('change', (evt) => {
+    if (evt.target.id === 'housing-type') {
+      console.log('Тип дома');
+    }
+    if (evt.target.id === 'housing-price') {
+      console.log('Стоимость аренды');
+    }
+    if (evt.target.id === 'housing-rooms') {
+      console.log('Количество комнат');
+    }
+    if (evt.target.id === 'housing-guests') {
+      console.log('Количество гостей');
+    }
+    if (evt.target.parentElement.id === 'housing-features') {
+      console.log('Фишечки');
+    }
+  });
+};
+
+export {initFilterEventListener};
