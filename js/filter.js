75bd307f984168567a3b93a3ca5bc2e860211a7a
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-27 20:09:51 +0400

    Добавлены функции filterType и filterPrice

--- js/filter.js
+++ js/filter.js
@@ -2,14 +2,41 @@ import {upgradeLayer} from './map.js';
 
 const ANOTHER_ADS = 10;
 
-const filterForm = document.querySelector('.map__filters');
 
-const getFilterPlaces = (offers) => {
+const filterForm = document.querySelector('.map__filters');
+const typeElement = filterForm.querySelector('#housing-type');
+const priceElement = filterForm.querySelector('#housing-price');
+
+const filterType = (place) => {
+  if (typeElement.value === place.offer.type || typeElement.value === 'any') {
+    return true;
+  }
+  return false;
+};
+
+const filterPrice = (place) => {
+  if (priceElement.value === 'low' && place.offer.price < 10000) {
+    return true;
+  }
+  if (priceElement.value === 'middle' && place.offer.price >= 10000 && place.offer.price <= 50000) {
+    return true;
+  }
+  if (priceElement.value === 'high' && place.offer.price >= 50000) {
+    return true;
+  }
+  if (priceElement.value === 'any') {
+    return true;
+  }
+  return false;
+};
+
+const getFilterPlaces = (places) => {
   const filteredPlaces = [];
-  for (const offer of offers) {
-    const isFiltered = filterType(offer) && filterPrice(offer) && filterRooms(offer) && filterGuests(offer) && filterFeatures(offer);
+  for (const place of places) {
+    // const isFiltered = filterType(offer) && filterPrice(offer) && filterRooms(offer) && filterGuests(offer) && filterFeatures(offer);
+    const isFiltered = filterType(place) && filterPrice(place);
     if (isFiltered) {
-      filteredPlaces.push(offer);
+      filteredPlaces.push(place);
     }
 
     if (filteredPlaces.length >= ANOTHER_ADS) {
