da465f054a2a9948f9eb0fee60b2e00571c8806b
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-27 20:27:32 +0400

    Добавлена функция filterRooms

--- js/filter.js
+++ js/filter.js
@@ -6,12 +6,12 @@ const ANOTHER_ADS = 10;
 const filterForm = document.querySelector('.map__filters');
 const typeElement = filterForm.querySelector('#housing-type');
 const priceElement = filterForm.querySelector('#housing-price');
+const roomElement = filterForm.querySelector('#housing-rooms ');
 
 const filterType = (place) => {
   if (typeElement.value === place.offer.type || typeElement.value === 'any') {
     return true;
   }
-  return false;
 };
 
 const filterPrice = (place) => {
@@ -27,14 +27,22 @@ const filterPrice = (place) => {
   if (priceElement.value === 'any') {
     return true;
   }
-  return false;
+};
+
+const filterRooms = (place) => {
+  if (+roomElement.value === place.offer.rooms) {
+    return true;
+  }
+  if (roomElement.value === 'any') {
+    return true;
+  }
 };
 
 const getFilterPlaces = (places) => {
   const filteredPlaces = [];
   for (const place of places) {
     // const isFiltered = filterType(offer) && filterPrice(offer) && filterRooms(offer) && filterGuests(offer) && filterFeatures(offer);
-    const isFiltered = filterType(place) && filterPrice(place);
+    const isFiltered = filterType(place) && filterPrice(place) && filterRooms(place);
     if (isFiltered) {
       filteredPlaces.push(place);
     }
