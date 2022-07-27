711204415a77f60b306c3a6f927587cee9625197
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-27 22:41:39 +0400

    Добавлен debounce, дописаны остальные фильтры

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,5 @@
 import {upgradeLayer} from './map.js';
+import {debounce} from './util.js';
 
 const ANOTHER_ADS = 10;
 
@@ -6,7 +7,10 @@ const ANOTHER_ADS = 10;
 const filterForm = document.querySelector('.map__filters');
 const typeElement = filterForm.querySelector('#housing-type');
 const priceElement = filterForm.querySelector('#housing-price');
-const roomElement = filterForm.querySelector('#housing-rooms ');
+const roomElement = filterForm.querySelector('#housing-rooms');
+const guestsElement = filterForm.querySelector('#housing-guests');
+const featureElements = Array.from(filterForm.querySelectorAll('.map__checkbox'));
+
 
 const filterType = (place) => {
   if (typeElement.value === place.offer.type || typeElement.value === 'any') {
@@ -38,11 +42,37 @@ const filterRooms = (place) => {
   }
 };
 
+const filterGuests = (place) => {
+  if (place.offer.guests < 1) {
+    return true;
+  }
+  if (+guestsElement.value === place.offer.guests) {
+    return true;
+  }
+  if (guestsElement.value === 'any') {
+    return true;
+  }
+};
+
+// Здесь лагает
+const filterFeatures = (place) => {
+  featureElements.every((element) => {
+    if (element.checked) {
+      return place.offer.features && place.offer.features.some((value) => value === element.value);
+    }
+    return true;
+  });
+};
+
 const getFilterPlaces = (places) => {
   const filteredPlaces = [];
   for (const place of places) {
-    // const isFiltered = filterType(offer) && filterPrice(offer) && filterRooms(offer) && filterGuests(offer) && filterFeatures(offer);
-    const isFiltered = filterType(place) && filterPrice(place) && filterRooms(place);
+    const isFiltered = filterType(place) &&
+      filterPrice(place) &&
+      filterRooms(place) &&
+      filterGuests(place) &&
+      // Если отключить filterFeatures, то всё работает
+      filterFeatures(place);
     if (isFiltered) {
       filteredPlaces.push(place);
     }
@@ -56,10 +86,10 @@ const getFilterPlaces = (places) => {
 };
 
 const initMapFilters = (places, group) => {
-  filterForm.addEventListener('change', () => {
+  filterForm.addEventListener('change', debounce(() => {
     const filteredPlaces = getFilterPlaces(places);
     upgradeLayer(group, filteredPlaces);
-  });
+  }));
 };
 
 export {initMapFilters};
