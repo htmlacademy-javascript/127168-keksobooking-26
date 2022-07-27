7cff4db715cb99d00d29e604b6f61cc59f0aeb13
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-27 12:21:41 +0400

    В обработчике событий отслеживается элементы, которые изменяются

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,20 @@
 const filterForm = document.querySelector('.map__filters');
 
 filterForm.addEventListener('change', (evt) => {
-  console.dir(evt.target);
+  if (evt.target.id === 'housing-type') {
+    console.log('Тип дома');
+  }
+  if (evt.target.id === 'housing-price') {
+    console.log('Стоимость аренды');
+  }
+  if (evt.target.id === 'housing-rooms') {
+    console.log('Количество комнат');
+  }
+  if (evt.target.id === 'housing-guests') {
+    console.log('Количество гостей');
+  }
+  if (evt.target.parentElement.id === 'housing-features') {
+    console.log('Фишечки');
+  }
+  // console.dir(evt.target);
 });
