f1951c40b99f980e01b413bd824ecaf4ee467ec7
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2022-07-31 01:40:25 +0400

    Значения поля стоимости синхронизировано со слайдером. Добавлен debounce, чтобы не дребезжало

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,9 @@
 import {pristine} from './validation.js';
+import {debounce} from './util.js';
 
 const RESET_PRICE_VALUE = 1000;
+const DEBOUNCE_TIMEOUT = 300;
+const MAX_PRICE = 100000;
 
 const adForm = document.querySelector('.ad-form');
 const priceSliderElement = adForm.querySelector('.ad-form__slider');
@@ -11,10 +14,10 @@ priceField.value = RESET_PRICE_VALUE;
 noUiSlider.create(priceSliderElement, {
   range: {
     min: 0,
-    max: 100000,
+    max: MAX_PRICE,
   },
   start: RESET_PRICE_VALUE,
-  step: 100,
+  step: 1,
   connect: 'lower',
   format: {
     to: (value) => value.toFixed(0),
@@ -26,6 +29,12 @@ priceSliderElement.noUiSlider.on('update', () => {
   priceField.value = priceSliderElement.noUiSlider.get();
   pristine.validate(priceField);
 });
+
+priceField.addEventListener('input', debounce(() => {
+  if (priceField.value <= MAX_PRICE) {
+    priceSliderElement.noUiSlider.set(priceField.value);
+  }
+}, DEBOUNCE_TIMEOUT));
 
 adForm.addEventListener('reset', () => {
   priceSliderElement.noUiSlider.set(RESET_PRICE_VALUE);
